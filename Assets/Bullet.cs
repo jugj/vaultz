7a571a37aa250f83f1b04fccb6b7186fdebbbb69
Alex <jugj@users.noreply.github.com>
2026-07-18 19:36:37 +0200

    Änderungen von Alex am 2026-07-18 19:36:37

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,12 +1,24 @@
-void OnTriggerEnter2D(Collider2D other)
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int schaden = 5;
+    Vector2 richtung;
+    void OnTriggerEnter2D(Collider2D other)
 {
     Debug.Log("Kugel trifft: " + other.gameObject.name);
 
-    leben hp = other.GetComponent<leben>();
+    leben hp = other.gameObject.GetComponent<leben>();
 
     if (hp != null)
     {
         hp.Schaden(schaden);
         Debug.Log("Schaden gemacht!");
     }
+}
+public void SetRichtung (Vector2 richtung){
+this.richtung = richtung;
+}
+
+
 }
