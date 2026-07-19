4c728a60b524fe87a0775cc8be0beade497d4d2a
esads <jugj@users.noreply.github.com>
2026-07-19 11:06:46 +0200

    Änderungen von esads am 2026-07-19 11:06:46

--- Assets/gegnerbewegung.cs
+++ Assets/gegnerbewegung.cs
@@ -8,7 +8,7 @@ public float Geschwindigkeit = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target=GameObject.FindWithTag("Tresor").GetComponent<Transform>();
     }
 
     // Update is called once per frame
