5d170f43446178b5758e1f97c3dfdaa1b4072f89
Alex <jugj@users.noreply.github.com>
2026-07-19 14:26:18 +0200

    release

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-GameJam - Die Schlucht-1.0";
 const contentToCache = [
-    "Build/docs.loader.js",
-    "Build/docs.framework.js",
-    "Build/docs.data",
-    "Build/docs.wasm",
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/4cd31e5a7bc834ec9e92833b2dc4907b.framework.js",
+    "Build/238221391a772459e36119a2c4aaa63f.data",
+    "Build/723dbaf9bafa274a7ccf25acc079c792.wasm",
     "TemplateData/style.css"
 
 ];
